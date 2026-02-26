55af49d08e347f1e304c0c6fe36ec81520451b5a
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2026-02-26 16:39:49 -0300

    Melhorando cor de fundos das redes sociais do rodapé

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,25 @@
 import "./Footer.styles.css";
 
 import github from "../../assets/icons/github.png";
+import whatsApp from "../../assets/icons/whatsApp.png";
+import instagram from "../../assets/icons/instagram.png";
 
 export function Footer() {
   return (
     <footer className="footer">
       <div className="social">
-        <a href="#" aria-label="GitHub">
+        <a href="https://github.com/GabrielDario" target="_blank" aria-label="GitHub">
           <img src={github} alt="GitHub" />
         </a>
 
-        <a href="#" aria-label="WhatsApp">
-          <img src={github} alt="WhatsApp" />
+        <a href="https://wa.me/5548991911269" target="_blank" aria-label="WhatsApp">
+          <img src={whatsApp} alt="WhatsApp" />
         </a>
 
-        <a href="#" aria-label="Email">
-          <img src={github} alt="Email" />
+        <a href="https://www.instagram.com/gabrieldario55/" target="_blank" aria-label="Instagram">
+          <img src={instagram} alt="Instagram" />
         </a>
+
       </div>
 
       <p>Developed by Gabriel Dario</p>
